2a51a360a64347e616371266bd7ca5245d438b4a
AnanjayGurjar <ananjaygurjar@outlook.com>
2022-06-25 13:28:23 +0530

    routes and db setup

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const noteRouter = require('./routes/noteRoutes');
+const userRouter = require('./routes/userRoutes');
 const app = express();
+require('dotenv').config();
+
+app.use('/users', userRouter);
+app.use('/notes', noteRouter);
 
 app.get('/', (req, res) => {
     res.send("Get request at home route");
 
 })
+
+
 
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = require('./app')
 
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
-app.listen(process.env.PORT, () =>{
-    console.log(`Server is running at port ${process.env.PORT}`)
-});
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("Connection with database is successful");
+    app.listen(process.env.PORT, () =>{
+        console.log(`Server is running at port ${process.env.PORT}`)
+    });
+})
+.catch((error) => {
+    console.log("Failed to connect with database");
+    console.log(error);
+})
+
